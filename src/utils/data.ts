92c1411ed8c2b8fe5ec097652ef6794853dc3a38
Sumit Thakur <sumitthakur5044@gmail.com>
2024-04-09 03:27:05 +0530

    commitment here

---
+++ src/utils/data.ts
@@ -0,0 +1,46 @@
+const services = [
+  {
+    title: "Immersive Multilingual Experience",
+    description:
+      "Dive into a world of languages! Explore poems, music, and stories from diverse cultures, all presented in multiple languages to foster global understanding and language acquisition.",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+  },
+  {
+    title: "Interactive Learning Adventures",
+    description:
+      "Embark on interactive journeys! Our AI-driven animations and captivating stories turn learning into exciting adventures, encouraging children to actively engage with educational content.",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+  },
+  {
+    title: "Personalized Learning Pathways",
+    description:
+      "Tailored learning experiences for every child! Our platform adapts to individual learning styles and preferences, providing personalized recommendations and activities to optimize educational growth.",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+  },
+  {
+    title: "Parental Engagement & Insights",
+    description:
+      "Stay connected and involved in your child's learning journey! Gain insights into your child's progress, explore recommended activities, and actively participate in their educational development",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+  },
+  {
+    title: "Creative Expression & Exploration",
+    description:
+      "Unleash creativity and imagination! Encourage your child to express themselves through art, music, and storytelling, fostering a love for creativity and exploration.",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+  },
+  {
+    title: "Continuous Learning & Expansion",
+    description:
+      "Never stop learning! Access a constantly expanding library of educational content, covering a wide range of subjects and topics to fuel curiosity and lifelong learning.",
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+  },
+];
+
+export { services };
